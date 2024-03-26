dc83a626db2f8689ecc5cb0625df947b7bf8f935
MariusSilenskis <marius.silenskis@gmail.com>
2024-03-26 20:00:13 +0100

    updated project name

--- config.ts
+++ config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export default {
   ERC20: process.env.ERC20,
   OWNER: process.env.OWNER,
-  RECOVERY_WALLET: process.env.RECOVERY_WALLET
+  RECOVERY_WALLET: process.env.RECOVERY_WALLET,
 };
