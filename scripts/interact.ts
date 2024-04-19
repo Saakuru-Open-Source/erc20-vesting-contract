1b206de7e8831439449308efda65045e3e26892e
MariusSilenskis <marius.silenskis@gmail.com>
2024-04-19 13:17:26 +0200

    added script for testing

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -1,6 +1,8 @@
 import * as dotenv  from 'dotenv';
-import { getContracts } from './utils/setup';
+import { getContracts, txConfig } from './utils/setup';
 import { BigNumber } from 'ethers';
+import moment from 'moment';
+import { ethers } from 'hardhat';
 
 dotenv.config();
 
@@ -10,7 +12,32 @@ const main = async () => {
 
   const contracts = getContracts();
 
-  await contracts.vesting.getAvailableWithdrawAmountForAddress(process.env.OWNER);
+
+  
+  // console.log('allowance', (await contracts.erc20.allowance(process.env.OWNER, contracts.vesting.address)).div(BigNumber.from(10).pow(18)).toString());
+
+  // const allowanceTx = await contracts.erc20.approve(contracts.vesting.address, BigNumber.from(10).pow(18).mul(1000000), txConfig);
+
+  // console.log(await allowanceTx.wait());
+
+  // console.log('allowance', (await contracts.erc20.allowance(process.env.OWNER, contracts.vesting.address)).div(BigNumber.from(10).pow(18)).toString());
+
+  const startTimestamp = moment().add(2, 'minutes').unix();
+  const walletNew = ethers.Wallet.createRandom();
+
+  const tx = await contracts.vesting.createVestingSchedule(
+    walletNew.address,
+    BigNumber.from(10).pow(18).mul(1231),
+    startTimestamp,
+    3,
+    txConfig,
+  );
+
+  console.log(await tx.wait());
+
+  const vestingSchedule = await contracts.vesting.vestingScheduleForBeneficiary(walletNew.address);
+
+  console.log(vestingSchedule);
 };
 
 main()
--- scripts/utils/setup.ts
+++ scripts/utils/setup.ts
@@ -1,13 +1,14 @@
 import * as ethers from 'ethers';
 import * as dotenv  from 'dotenv';
 import { networks } from '../../helpers/networks';
-import { VestingContract } from '../../dist/types';
+import { ERC20, VestingContract } from '../../dist/types';
 
 dotenv.config();
 
 console.log('Running... ', process.env.NETWORK);
 
 const vesting = require(`../../deployments/${process.env.NETWORK}/VestingContract.json`);
+const erc20Abi = require('../../abi/contracts/mocks/ERC20.sol/MockERC20.json');
 
 export const deployments = {
   vesting,
@@ -21,6 +22,7 @@ export const wallet = new ethers.Wallet(networks[process.env.NETWORK || '0'].acc
 export const getContracts = () => {
   return {
     vesting: new ethers.Contract(vesting.address, vesting.abi, wallet) as VestingContract,
+    erc20: new ethers.Contract(process.env.ERC20, erc20Abi, wallet) as ERC20,
   };
 };
 
